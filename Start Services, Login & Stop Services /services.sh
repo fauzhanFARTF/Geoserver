f1764d3eebd3e260e3509b800e784ae52253bc1f
Fauzan Nurrachman <62664091+fauzhanFARTF@users.noreply.github.com>
2023-09-02 15:04:12 +0700

    Start Services, Login & Stop Services

---
+++ Start Services, Login & Stop Services /services.sh
@@ -0,0 +1,14 @@
+# Khusus Pengguna Mac
+    # Start Services 
+    di terminal Masuk ke dalam folder "/usr/local/geoserver/bin"
+    dengan mengetikkan perintah di terminal
+        "cd /usr/local/geoserver/bin"
+
+    Jalankan services dengan mengetikkan perintah di terminal
+        "sh startup.sh"
+
+    untuk melihat isi dari folder "/usr/local/geoserver/bin" dengan mengetikkan perintah di terminal
+        "ls"
+
+    untuk menstop service cukup mengetikkan perintah
+        "ctrl+c"
---
+++ Start Services, Login & Stop Services /username & password.sh
@@ -0,0 +1,3 @@
+# Default
+username : admin
+password : geoserver
